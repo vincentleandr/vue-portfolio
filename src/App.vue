50cd3706626ea5606c40419d155a6b3b1da6dd97
vincentleandr <31660411+vincentleandr@users.noreply.github.com>
2018-05-31 23:23:52 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    Hello World
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
