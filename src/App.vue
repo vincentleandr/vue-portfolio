13060775244c046ffc9959a94c3a9ef916d7adc8
vincentleandr <31660411+vincentleandr@users.noreply.github.com>
2018-06-05 22:28:05 +0700

    .

--- src/App.vue
+++ src/App.vue
@@ -20,8 +20,22 @@
 			
 		</section>
 
-		<div :class="['menu-overlay d-flex j-center a-center', {'active' : menuActive}]">
-			<h2>Menu</h2>
+		<div :class="['menu-overlay d-flex a-center', {'active' : menuActive}]">
+			<div class="menu d-flex-col">
+				<nav class="menu-nav">
+					<ul class="d-flex-col">
+						<li><a class="menu-nav__option" href="">Work</a></li>
+						<li><a class="menu-nav__option" href="">About</a></li>
+						<li><a class="menu-nav__option" href="">Contact</a></li>
+					</ul>
+				</nav>
+				
+			</div>
+			<div class="menu-social d-flex j-end">
+				<a class="menu-social__option" href="">vincentleander@yahoo.com</a>
+				<a class="menu-social__option" href="">Github</a>
+				<a class="menu-social__option" href="">LinkedIn</a>
+			</div>
 		</div>
 	</div>
 </template>
