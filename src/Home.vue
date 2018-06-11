81eeb8e423d7ecc8d8e5fcd3ee3d3316359b64f6
vincentleandr <31660411+vincentleandr@users.noreply.github.com>
2018-06-11 20:29:44 +0700

    Will be continued...

--- src/Home.vue
+++ src/Home.vue
@@ -1,9 +1,11 @@
 <template>
     <div class="main-content main-content--fullpage d-flex a-center animated fadeIn">
         <div class="d-flex-col">
-            <span class="home-text-small">Hello<span style="color:#FBB2AD;">.</span></span>
-            <h1 class="home-text-big"><span class="home-text-small">My name is </span>Vincent Leander<span class="home-text-small">,</span></h1>
-            <span class="home-text-big"><span class="home-text-small">I'm a </span>front end developer.</span>
+            <h1 class="home-text-big">
+                Hello. <br>
+                I am Vincent Leander <br>
+                A front end developer.
+            </h1>
         </div>
     </div>
 </template>
