3a250ce492f587da8794e56cc78743abdb8b8873
vincentleandr <31660411+vincentleandr@users.noreply.github.com>
2018-06-19 22:11:10 +0700

    .

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -2,22 +2,24 @@
     <div class="main-content main-content--fullpage d-flex a-center animated fadeIn">
         <div class="d-flex d-flex--col">
             <h1>Say Hi!</h1>
-            <p>If you'd like to talk about projects, my work, opportunities <br>
+            <p class="contact-desc">If you'd like to talk about projects, my work, opportunities <br>
             or anything else then feel free to hit me up.</p>
 
-            <div class="d-flex j-sp-between">
+            <div class="contact-info d-flex j-sp-between a-end">
                 <div class="d-flex d-flex--col">
                     <span class="span-label">Email</span>
-                    <span class="span-basic">vincentleander@yahoo.com</span>
+                    <span class="span-basic"><a class="contact-info__link" href="">vincentleander@yahoo.com</a></span>
                 </div>
                 <div class="d-flex d-flex--col">
                     <span class="span-label">Phone</span>
-                    <span class="span-basic">+62 818 087 315 90</span>
+                    <span class="span-basic"><a class="contact-info__link" href="">+62-8180-8731-590</a></span>
                 </div>
+                
                 <div class="d-flex j-sp-between">
-                    <span><i class="fa fa-github"></i></span>
-                    <span><i class="fa fa-linkedin"></i></span>
+                    <a class="menu-social__option contact-info__link" href=""><i class="fa fa-github"></i></a>
+                    <a class="menu-social__option contact-info__link" href=""><i class="fa fa-linkedin"></i></a>
                 </div>
+                
             </div>
         </div>
     </div>
