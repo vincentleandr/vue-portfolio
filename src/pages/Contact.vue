4b05c8975acefb20f9ff39fed4ccbeb5c84e077f
vincentleandr <31660411+vincentleandr@users.noreply.github.com>
2018-07-07 20:46:43 +0700

    .

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,22 +1,23 @@
 <template>
     <div class="main-content main-content--fullpage d-flex a-center animated fadeIn">
         <div class="d-flex d-flex--col">
-            <h2>Say Hi!</h2>
-            <p class="contact-desc">If you'd like to talk about projects, my work, opportunities <br>
-            or anything else then feel free to hit me up.</p>
+            <h2>Say Hi</h2>
 
-            <div class="contact-info d-flex d-flex--col">
-                <div class="contact-info__det d-flex d-flex--col">
-                    <span class="span-label">Email</span>
+            <div class="d-flex j-sp-between">
+                <div class="contact-info d-flex d-flex--col">
+                    <p>Want to talk about projects or something else?</p>
                     <a class="contact-info__link" href="mailto:vincentleander@yahoo.com">vincentleander@yahoo.com</a>
                 </div>
-                <div class="contact-info__det d-flex d-flex--col">
-                    <span class="span-label">Phone</span>
+                <div class="contact-info d-flex d-flex--col">
+                    <p>Feel free to send me an email or a message.</p>
                     <a class="contact-info__link" href="tel:+6281808731590">+62-8180-8731-590</a>
                 </div>
-                <div class="contact-info__det d-flex">
-                    <a class="menu-social__option contact-info__link" href="https://github.com/vincentleandr" target="_blank"><i class="fa fa-github"></i></a>
-                    <a class="menu-social__option contact-info__link" href="" target="_blank"><i class="fa fa-linkedin"></i></a>
+                <div class="contact-info d-flex d-flex--col">
+                    <p>You can also find me on other platform like:</p>
+                    <div class="contact-info__det d-flex">
+                        <a class="menu-social__option contact-info__link" href="https://github.com/vincentleandr" target="_blank"><i class="fa fa-github"></i></a>
+                        <a class="menu-social__option contact-info__link" href="" target="_blank"><i class="fa fa-linkedin"></i></a>
+                    </div>
                 </div>
             </div>
         </div>
