7fe42ddf603ce145b3d9a63d9b8065871d3c465e
vincentleandr <31660411+vincentleandr@users.noreply.github.com>
2018-07-15 16:02:46 +0700

    .

--- src/pages/Work.vue
+++ src/pages/Work.vue
@@ -11,7 +11,50 @@
                 </parallax-element>
                 
                 <div class="project-details">
-                    <h2 class="project-details__name">Project Name</h2>
+                    <h2 class="project-details__name">TV-Maze Search</h2>
+                    <p>Bacon ipsum dolor amet shank cupim ribeye, corned beef meatball ham hock frankfurter hamburger spare ribs sausage andouille ground round.</p>
+                    <!--
+                    -->
+                    <div class="d-flex a-center">
+                        <div class="project-details__icon-tech d-flex j-center a-center">
+                            <img src="../assets/icons/html5.svg" alt="HTML 5">
+                        </div>
+                        <div class="project-details__icon-tech d-flex j-center a-center">
+                            <img src="../assets/icons/sass.svg" alt="HTML 5">
+                        </div>
+                        <div class="project-details__icon-tech d-flex j-center a-center">
+                            <img src="../assets/icons/reactjs.svg" alt="HTML 5">
+                        </div>
+                        <div class="project-details__icon-tech d-flex j-center a-center">
+                            <img src="../assets/icons/gulpjs.svg" alt="HTML 5">
+                        </div>
+                        <div class="project-details__icon-tech d-flex j-center a-center">
+                            <img src="../assets/icons/webpack.svg" alt="HTML 5">
+                        </div>
+                    </div>
+                    
+                    <div class="d-flex">
+                        <a class="btn btn--main project-details__btn" href="#">Visit</a>
+                        <a class="btn btn--secondary project-details__btn" href="#">View Code</a>
+                    </div>
+
+                </div>
+
+            </div>
+        </parallax-container>
+
+        <parallax-container class="d-flex d-flex--col works-page">
+            <div class="d-flex j-sp-between">
+                <parallax-element class="mac-border mac-border--bg-2" :parallaxStrength="-15" :type="'translation'">
+                    <div class="mac-border__top d-flex a-center">
+                        <div class="mac-border__btn mac-border__btn--red"></div>
+                        <div class="mac-border__btn mac-border__btn--orange"></div>
+                        <div class="mac-border__btn mac-border__btn--green"></div>
+                    </div>
+                </parallax-element>
+                
+                <div class="project-details">
+                    <h2 class="project-details__name">My Scholarships</h2>
                     <p>Bacon ipsum dolor amet shank cupim ribeye, corned beef meatball ham hock frankfurter hamburger spare ribs sausage andouille ground round.</p>
                     <!--
                     -->
